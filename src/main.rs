eed7a332c6232b2c757e52f8f3f8ef0a9fddf527
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2026-02-22 09:28:57 -0800

    Follow more advanced hello world tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,47 @@
 use eframe::egui;
 
-fn main() /* -> eframe::Result */ {
+fn main() -> eframe::Result {
 	println!("woooopaint will now be real.");
-	eframe::run_native("woooopaint",
-		eframe::NativeOptions::default(),
-		Box::new(|cc| Ok(Box::new(App::new(cc))))
+	return eframe::run_native("woooopaint",
+		eframe::NativeOptions {
+			viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 600.0]),
+			..Default::default()
+		},
+		Box::new(|cc| {
+			egui_extras::install_image_loaders(&cc.egui_ctx);
+			Ok(Box::<App>::default())
+		})
 	);
 }
 
-#[derive(Default)]
-struct App {}
+struct App {
+	image_name: String,
+	color: egui::Color32
+}
 
-impl App {
-	fn new(cc: &eframe::CreationContext<'_>) -> Self {
-		Self::default()
+impl Default for App {
+	fn default() -> Self {
+		Self {
+			image_name: "Default Image".to_owned(),
+			color: egui::Color32::from_rgb(255, 255, 255)
+		}
 	}
 }
 
 impl eframe::App for App {
-	fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+	fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
 		egui::CentralPanel::default().show(ctx, |ui| {
-			ui.label("Hello Label");
-			ui.heading("Hello Heading");
+			ui.heading(format!("Image: {}", self.image_name));
+			ui.horizontal(|ui| {
+				let label = ui.label("Change image name:");
+				ui.text_edit_singleline(&mut self.image_name).labelled_by(label.id);
+			});
+			if ui.button("Change Color").clicked() {
+				self.color = egui::Color32::from_rgb(rand::random(), rand::random(), rand::random());
+			}
+			// Color picker
+			ui.color_edit_button_srgba(&mut self.color);
+			ui.image(egui::include_image!("../crimsoncode-sally.png"));
 		});
 	}
 }

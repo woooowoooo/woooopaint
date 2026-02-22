d789c6fc158e70419520698d952d684d3a6562ab
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2026-02-22 06:37:24 -0800

    Follow doc-given hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
+use eframe::egui;
+
+fn main() /* -> eframe::Result */ {
 	println!("woooopaint will now be real.");
+	eframe::run_native("woooopaint",
+		eframe::NativeOptions::default(),
+		Box::new(|cc| Ok(Box::new(App::new(cc))))
+	);
+}
+
+#[derive(Default)]
+struct App {}
+
+impl App {
+	fn new(cc: &eframe::CreationContext<'_>) -> Self {
+		Self::default()
+	}
+}
+
+impl eframe::App for App {
+	fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+		egui::CentralPanel::default().show(ctx, |ui| {
+			ui.label("Hello Label");
+			ui.heading("Hello Heading");
+		});
+	}
 }
